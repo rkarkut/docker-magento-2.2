1d294a69fd1d1cf40641c396b2115d49eb1d610a
Radosław Karkut <radoslaw.karkut@getresponse.com>
2017-11-03 19:34:48 +0100

    docker for Magento 2.2

---
+++ web/app/etc/env.php
@@ -0,0 +1,60 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'e3fe11782c5bbebe57d2486aa1f9db76',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'magento_22_mysql',
+        'dbname' => 'magento',
+        'username' => 'dev',
+        'password' => 'dev',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'session' => 
+  array (
+    'save' => 'db',
+  ),
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+    'compiled_config' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Fri, 03 Nov 2017 17:57:53 +0000',
+  ),
+);
